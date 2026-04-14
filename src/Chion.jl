9140960dd42c70c286a03cb0f2bc30725b9d27ee
NilsBochow <contact+github@nilsbochow.com>
2026-04-14 14:17:58 +0200

    refactor(module): restructure Chion module and remove SnowpackModel
    - Consolidate includes into Chion.jl
    - Remove SnowpackModel.jl and case_api.jl
    - Update exports in Chion module

--- src/Chion.jl
+++ src/Chion.jl
@@ -1,11 +1,40 @@
 module Chion
 
 using Printf
+using Dates
+using HDF5
+using Base.Threads: @threads
+using Adapt: Adapt, adapt, @adapt_structure
+using CUDA
+using Enzyme
+using KernelAbstractions
 
-include("SnowpackModel.jl")
-using .SnowpackModel
+include("model_constants.jl")
+include("domain_interfaces.jl")
+include("execution_backends.jl")
+include("snowpack_domain.jl")
+include("step_forcing.jl")
+include("step_scratch.jl")
+include("column_state_utils.jl")
+include("processes/albedo.jl")
+include("processes/layer_structure.jl")
+include("processes/accumulation.jl")
+include("processes/melt.jl")
+include("processes/diurnal_shortwave.jl")
+include("processes/surface_fluxes.jl")
+include("processes/energy_flux.jl")
+include("processes/densification.jl")
+include("processes/percolation.jl")
+include("processes/refreezing.jl")
+include("domain_summaries.jl")
+include("step_timing.jl")
+include("step.jl")
+include("step_field_batches.jl")
+include("state_access.jl")
 include("case_runtime.jl")
-include("case_api.jl")
+include("cases/definitions.jl")
+include("cases/forcing_file_definition.jl")
+include("cases/api.jl")
 
 export step!
 export SnowpackDomain
@@ -15,13 +44,13 @@ export SnowpackPhysicalConstants
 export RunConfig, RunResult
 export SnowpackCase
 export CASE_NETCDF_VARIABLE_GROUPS, CASE_NETCDF_VARIABLES
-export TimingStats, physics
+export StepTimingStats, physics
 export prescribed_case, synthetic_case, run_case
 export StepWorkspace, threaded_workspaces
 export ColumnarStepWorkspace
 export continuous_bottom_deplete!
 export update_surface_albedo!
 export get_state, print_state
-export StepTimingStats, add_timing!, timing_rows, print_timing_summary
+export add_timing!, timing_rows, print_timing_summary
 
 end
--- src/case_api.jl
+++
@@ -1,7 +0,0 @@
-using Dates
-using HDF5
-using Base.Threads: @threads
-
-include("cases/definitions.jl")
-include("cases/forcing_file_definition.jl")
-include("cases/api.jl")
